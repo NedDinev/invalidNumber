9acb1b953cfc420d6d997af4a1d6ffa5b7f2ecd7
Nedelcho Dinev <ned.dinev@gmail.com>
2021-10-25 20:30:27 +0300

    Add files via upload

---
+++ invalidNumber.js
@@ -0,0 +1,18 @@
+function invalidNumber(input) {
+  input = Number(input);
+
+  if ((input >= 100) & (input <= 200) || input == 0) {
+    console.log();
+  } else {
+    console.log("Invalid");
+  }
+}
+
+invalidNumber(75);
+invalidNumber(150);
+invalidNumber(220);
+invalidNumber(199);
+invalidNumber(-1);
+invalidNumber(100);
+invalidNumber(200);
+invalidNumber(0);
